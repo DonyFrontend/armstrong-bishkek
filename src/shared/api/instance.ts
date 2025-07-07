cfa984a92f173c0bc6cd4787fcc5495032818f47
Doni <donibro357@gmail.com>
2025-07-07 18:51:58 +0600

    fix url #2

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = "https://heavy-rules-behave.loca.lt/";
+export const api = "https://heavy-rules-behave.loca.lt";
 
 export const instance = axios.create({
   baseURL: api,
